655d6a428a7b73e9543d060a115ae062f594f023
JustinBenning <justin.benning@gmail.com>
2024-03-08 09:57:40 -0500

    upload of project

---
+++ Testing/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Testing.Models;
+using System;
+
+namespace Testing
+{
+    public interface IProductRepository
+    {
+        public IEnumerable<Product> GetAllProducts();
+        Product GetProduct(int id);
+        void UpdateProduct(Product product);
+        public void InsertProduct(Product productToInsert);
+        public IEnumerable<Category> GetCategories();
+        public Product AssignCategory();
+        public void DeleteProduct(Product product);
+    }
+}
